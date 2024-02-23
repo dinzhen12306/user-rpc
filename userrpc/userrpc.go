f66e106ec55337985e07f658a3c7d2ccf6f0b220
hkh <3478154431@qq.com>
2024-02-23 20:05:29 +0800

    first commit

--- userrpc/userrpc.go
+++ userrpc/userrpc.go
@@ -1,19 +1,19 @@
-package main
+package userrpc
 
 import (
+	"fmt"
 	"google.golang.org/grpc"
 	"log"
 	"net"
 	_ "userrpc/config"
 	_ "userrpc/model/mysql"
 	"userrpc/userclient"
-	pb "userrpc/userrpc"
 )
 
-func main() {
+func NewRpcServer(address int64) {
 	grpcServer := grpc.NewServer()
-	pb.RegisterStreamGreeterServer(grpcServer, new(userclient.UserRpcClient))
-	listen, err := net.Listen("tcp", ":8090")
+	RegisterStreamGreeterServer(grpcServer, new(userclient.UserRpcClient))
+	listen, err := net.Listen("tcp", fmt.Sprintf(":%d", address))
 	if err != nil {
 		log.Println("服务监听失败", err.Error())
 	}
